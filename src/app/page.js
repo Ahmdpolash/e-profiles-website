a9bd2c6cddb9cc2f58ea435c6c020bec9b808973
Polash <ahmedpolash732@gmail.com>
2024-03-23 07:19:12 -0600

    deploy issue resolved

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Banner from "@/Components/Home/Banner";
+import Footer from "@/Components/shared/Footer";
+import Navbar from "@/Components/shared/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Footer />
+    </>
+  );
+}
+ 
